cc2a7728045d7a3de7283b1d74c729c2eaece9b9
denisavl <denisavlad604@gmail.com>
2024-01-07 22:39:26 +0200

    Populate NailsPage

--- src/Pages/NailsPage/Nails.jsx
+++ src/Pages/NailsPage/Nails.jsx
@@ -1,7 +1,23 @@
 import CreatePage from '../CreatePage/CreatePage'
+import { fetchProducts } from '../../fetch'
+import { useQuery } from "@tanstack/react-query";
 
 export default function NailsPage(){
+    const nailsProductTypes = ["nail_polish"];
+
+    const nailsProducts = useQuery({
+        queryKey: ["face"],
+        queryFn: () => fetchProducts(nailsProductTypes),
+      });
+   
+      const products = nailsProducts.data?.successfulResults.flatMap(result => (
+        result.data.map(product => ({
+          ...product,
+          product_type: result.productType.replace(/_/g, ' ')
+        }))
+      )) || [];
+
     return (
-        <CreatePage />
+        <CreatePage title={"Nails"} products={products}/>
     )
 }
