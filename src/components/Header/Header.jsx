bbb2390bbef0173030d9a1a01f989379fa031431
denisavl <denisavlad604@gmail.com>
2024-01-08 14:55:54 +0200

    Implement navigation links in navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,34 +5,40 @@ import basketIcon from "../../assets/shopping-cart.png";
 const Header = () => {
   return (
     <div className={styles.headerContainer}>
-      {/* shop all, ten, ochi, buze, unghii, about us, logo, search, cos  */}
-      <div className={styles.buttons}>
-        <button type="button" className={styles.btn}>
-          Shop All
-        </button>
-        <button type="button" className={styles.btn}>
-          Face
-        </button>
-        <button type="button" className={styles.btn}>
-          Eyes
-        </button>
-        <button type="button" className={styles.btn}>
-          Lips
-        </button>
-        <button type="button" className={styles.btn}>
-          Nails
-        </button>
-        <button type="button" className={styles.btn}>
-          About Us
-        </button>
-      </div>
+      <ul className={styles.buttons}>
+        <li>
+          <a href="/shop" className={styles.btn}>Shop All</a>
+        </li>
+        <li>
+          <a href="/face" className={styles.btn}>Face</a>
+        </li>
+        <li>
+          <a href="/eyes" className={styles.btn}>Eyes</a>
+        </li>
+        <li>
+          <a href="/lips" className={styles.btn}>Lips</a>
+        </li>
+        <li>
+          <a href="/nails" className={styles.btn}>Nails</a>
+        </li>
+        <li>
+          <a href="/about" className={styles.btn}>About Us</a>
+        </li>
+      </ul>
       <div>
-        <img className={styles.logo} src={logoImg} alt="logo image" />
+        <a href="/">
+          <img className={styles.logo} src={logoImg} alt="logo image" />
+        </a>
+        
       </div>
       <div className={styles.rightSide}>
-        <div> 
+        <div>
           <form className={styles.formContainer}>
-          <img className={styles.searchIcon} src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='20' height='16' class='bi bi-search' viewBox='0 0 16 16'%3E%3Cpath d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z'%3E%3C/path%3E%3C/svg%3E" alt="" />
+            <img
+              className={styles.searchIcon}
+              src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='20' height='16' class='bi bi-search' viewBox='0 0 16 16'%3E%3Cpath d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z'%3E%3C/path%3E%3C/svg%3E"
+              alt=""
+            />
             <input
               type="text"
               name="search-bar"
